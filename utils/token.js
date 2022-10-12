3a33c3cf4d7b60239d7150ea4d187e2014b7fd31
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-10-12 13:44:55 +0600

    added some utils

---
+++ utils/token.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.generateToken = (userInfo)=>{
+    const payLoad = {
+        email:userInfo.email,
+        role:userInfo.role
+    };
+    const token = jwt.sign(payLoad, process.env.TOKEN,{
+        expiresIn:"30d"
+    })
+    return token
+}
+
