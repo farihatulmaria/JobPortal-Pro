37f645f35b624a1fff7c5c2464a681b06579f45a
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-10-12 13:44:20 +0600

    added some middlewares

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require("util");
+module.exports.verifyToken = async(req,res,next)=>{
+    try {
+        const token = req?.headers?.autorization.split(" ")[1];
+        if(!token){
+            res.status(401).json({
+                status:'You shall not pass',
+                message:"You aren't logged in",
+                error:err.message
+            })
+        }
+        const decodedToken = await promisify(jwt.verify()(token,process.env.TOKEN));
+        const user = User.findOne({ email: decoded.email })
+
+        req.user = decoded;
+
+        next();
+
+    } catch (err) {
+        res.status(400).json({
+            status:'You shall not pass',
+            message:"you aren't a user",
+            error:err.message
+        })
+    }
+}
